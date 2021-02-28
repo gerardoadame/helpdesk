2bc8a0d113442a03c73bc7ff20489b095e49af74
gerardoadame <gera_skate@outlook.com>
2021-02-27 21:25:28 -0600

    crear ticket listo

--- app/Http/Controllers/TicketController.php
+++ app/Http/Controllers/TicketController.php
@@ -11,8 +11,11 @@ class TicketController extends Controller
     {
         Ticket::create([
             'subject' => $request->get('subject'),
+            'time' => $request->get('time'),
             'description' => $request->get('description'),
             'image' => $request->get('image'),
+            'feedback' => $request->get('feedback'),
+            'technical_image' => $request->get('technical_image'),
             'employed_id' => $request->get('employed_id'),
             'status_id' => $request->get('status_id'),
             'type_id' => $request->get('type_id'),
