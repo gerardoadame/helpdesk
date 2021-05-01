2ee5011ae88f8bafc981be798252bb59f7929370
Juan Angel <juaangel.rey@gmail.com>
2021-04-30 23:30:40 -0500

    relation fix

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -26,7 +26,7 @@ class Person extends Model
 
     public function user()
     {
-        return $this->hasOne(User::class, "user_id", "id");
+        return $this->hasOne(User::class, "id", "user_id");
     }
 
     public function tickets_employed()
