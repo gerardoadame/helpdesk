daf44b395932df54979592585be6e1501e123671
gerardoadame <gera_skate@outlook.com>
2021-05-11 17:48:55 -0500

    actualizacion en los seeders y migraciones

--- database/migrations/2021_02_09_041209_create_user_table.php
+++ database/migrations/2021_02_09_041209_create_user_table.php
@@ -22,8 +22,8 @@ class CreateUserTable extends Migration
             $table->biginteger('type_id')->unsigned();
             $table->foreign('type_id')->references('id')->on('user_type');
             $table->text('avatar')->default('default.png');
-            $table->biginteger('user_id')->unsigned();
-            $table->foreign('user_id')->references('id')->on('Users');
+            $table->biginteger('person_id')->unsigned();
+            $table->foreign('person_id')->references('id')->on('Persons');
         });
     }
 
