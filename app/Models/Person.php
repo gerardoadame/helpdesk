bd477a486e199873b702b50b458662d06616430a
gerardoadame <gera_skate@outlook.com>
2021-05-11 21:41:48 -0500

    Correciones

--- app/Models/Person.php
+++ app/Models/Person.php
@@ -25,7 +25,7 @@ class Person extends Model
     ];
 
     public function user() {
-        return $this->hasOne(User::class);
+        return $this->belongsTo(User::class);
     }
 
     public function tickets_employed()
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,7 +45,7 @@ class User extends Authenticatable
     ];
 
     public function person() {
-        return $this->belongsTo(Person::class);
+        return $this->hasOne(Person::class);
     }
 
     public function type(){
