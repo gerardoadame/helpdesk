a42c2d5a52bb9a511af7fc086ee43efcd1a5535b
juaangel <juaangel.rey@gmail.com>
2021-05-19 15:33:43 -0500

    cors middleware

--- config/app.php
+++ config/app.php
@@ -2,10 +2,6 @@
 
 return [
 
-    header('Access-Control-Allow-Origin: *'),
-    header('Access-Control-Allow-Methods: *'),
-    header('Access-Control-Allow-Headers: Origin, X-Requested-With,Authorization, Content-Type, Accept'),
-
     /*
     |--------------------------------------------------------------------------
     | Application Name
