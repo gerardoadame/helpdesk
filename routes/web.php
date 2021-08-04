4deb03e8306e03da2e5e7791c2beeeeb6c8b5fa5
Angel <juaangel.rey@gmail.com>
2021-08-04 12:10:28 -0500

    optimizaciones

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,11 @@ use App\Models\Ticket;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
+| IMPORTANT!!!
+| * This app is an API, to add web routes you will need to uncomment some
+| services in the "web" middleware group
+| * Esta app es una API, para aniadir rutas web tendras que descomentar algunos
+| servicios en el grupo de middleware "web"
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () { return view('welcome'); });
