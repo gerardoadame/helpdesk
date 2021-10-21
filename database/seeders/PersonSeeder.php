65fa380a1777055d0d484f0b7e77d80775ba0d02
Juan Angel <juaangel.rey@gmail.com>
2021-10-21 18:22:19 -0500

    seeders ajustados al cambio de relación 1:1

--- database/seeders/PersonSeeder.php
+++ database/seeders/PersonSeeder.php
@@ -15,36 +15,31 @@ class PersonSeeder extends Seeder
     public function run()
     {
         DB::table('persons')->insert([
-        	'name'=>'juan',
-        	'last_name'=>'espinoza',
-        	'birth'=>'2000-01-01',
-        	'address'=>"av del desierto #525",
-        	'phone'=>"871xxxxxxx",
-        	'employment'=>"ingeniero",
-        	'area_id'=>1,
-            'user_id'=>1,
-        ]);
-
-        DB::table('persons')->insert([
-            'name'=>'Enrique',
-            'last_name'=>'de los santos',
-            'birth'=>'2000-01-01',
-            'address'=>"cerrada huizachal #78",
-            'phone'=>"871xxxxxxx",
-            'employment'=>"tecnico",
-            'area_id'=>1,
-            'user_id'=>2,
-        ]);
-
-        DB::table('persons')->insert([
-            'name'=>'angel',
-            'last_name'=>'lira',
-            'birth'=>'1999-03-15',
-            'address'=>"carolinas",
-            'phone'=>"871xxxxxxx",
-            'employment'=>"secretario",
-            'area_id'=>2,
-            'user_id'=>3,
+            [
+                'name' => 'Juan Enrique',
+                'last_name' => 'Espinoza De Los Santos',
+                'birth' => '2000-01-01',
+                'address' => "av del desierto #525",
+                'phone' => "871xxxxxxx",
+                'employment' => "ingeniero",
+                'area_id' => 1,
+            ], [
+                'name' => 'Juan Gerardo',
+                'last_name' => 'Adame Torres',
+                'birth' => '2000-01-01',
+                'address' => "cerrada huizachal #78",
+                'phone' => "871xxxxxxx",
+                'employment' => "tecnico",
+                'area_id' => 1,
+            ], [
+                'name' => 'Juan Angel',
+                'last_name' => 'Reyes Lira',
+                'birth' => '1999-03-15',
+                'address' => "carolinas",
+                'phone' => "871xxxxxxx",
+                'employment' => "chingon",
+                'area_id' => 2,
+            ]
         ]);
     }
 }
